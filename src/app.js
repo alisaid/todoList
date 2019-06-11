b87b9a668472da2e38d70363a1c4c1a6a271e6ba
alisaid <alisaid@hotmail.com>
2019-06-11 11:43:13 +0100

    first job

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
+ReactDOM.render(<TodoList />, document.getElementById("app"));
