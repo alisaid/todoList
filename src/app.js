566bd937f39350ea2a9b4203bc768dfcf8d46b64
alisaid <alisaid@hotmail.com>
2019-06-11 11:50:48 +0100

    unnecessary codes deleted

--- src/app.js
+++ src/app.js
@@ -3,22 +3,6 @@ import ReactDOM from "react-dom";
 import TodoList from "./components/TodoList";
 
 class App extends Component {
-  state = {
-    count: 0
-  };
-
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-
   render() {
     return (
       <div className="App">
